a570953a9059e11f1ba9cd9e3fdeab54e0281d86
Brunna Serafina <brunnaserafina@gmail.com>
2022-07-22 16:08:03 -0300

    Flashcards embaralhados + ao clicar na pergunta o flashcard correspondente aparece

--- src/components/Flashcards.js
+++ src/components/Flashcards.js
@@ -1,4 +1,25 @@
-export default function Flashcards() {
+import React from 'react';
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+
+const questions = [
+    { question: "O que é JSX?", answer: "Uma extensão de linguagem do JavaScript" },
+    { question: "O React é __ ", answer: "uma biblioteca JavaScript para construção de interfaces" },
+    { question: "Componentes devem iniciar com __ ", answer: "letra maiúscula" },
+    { question: "Podemos colocar __ dentro do JSX", answer: "expressões" },
+    { question: "O ReactDOM nos ajuda ____", answer: "interagindo com a DOM para colocar componentes React na mesma" },
+    { question: "Usamos o npm para __ ", answer: "gerenciar os pacotes necessários e suas dependências" },
+    { question: "Usamos props para __", answer: "passar diferentes informações para componentes" },
+    { question: "Usamos estado(state) para __ ", answer: "dizer para o React quais informações quando atualizadas devem renderizar a tela novamente" }
+];
+
+const mapeei = (questions.map((question, index) => (
+    <Card key={index} question={question.question} answer={question.answer} />
+)));
+
+const embaralhei = mapeei.sort(()=> Math.random() - 0.5);
+
+export default function Flashcards({ play, setPlay }) {
     return (
         <div className="flashcards-closed">
             <div className="logo-title">
@@ -7,27 +28,43 @@ export default function Flashcards() {
             </div>
 
             <ul>
-                <li className="flashcard">
-                    <p>Pergunta 1</p>
-                    <ion-icon name="play-outline"></ion-icon>
-                </li>
-                <li className="flashcard">
-                    <p>Pergunta 2</p>
-                    <ion-icon name="play-outline"></ion-icon>
-                </li>
-                <li className="flashcard">
-                    <p>Pergunta 3</p>
-                    <ion-icon name="play-outline"></ion-icon>
-                </li>
-                <li className="flashcard">
-                    <p>Pergunta 4</p>
-                    <ion-icon name="play-outline"></ion-icon>
-                </li>
+                {(numbers.map((number, index) => (
+                    <Flashcard key={index} number={number} />
+                )))}
             </ul>
 
             <div className="completed">
                 <p>0/4 CONCLUÍDOS</p>
             </div>
+        </div>
+    );
+}
+
+function Flashcard({ number }) {
+    const [play, setPlay] = React.useState(true);
+    
+    if (play === false) {
+        return (
+            <>
+                {embaralhei[number - 1]}
+            </>
+        );
+    }
+
+    return (
+        <li className="flashcard" onClick={() => setPlay(!play)}>
+            <p>Pergunta {number}</p>
+            <ion-icon name="play-outline" ></ion-icon>
+        </li>
+    );
+
+}
+
+function Card({ question, answer }) {
+    return (
+        <div className="card-front">
+            <h1>{question}</h1>
+            <img src="./assets/imgs/setinha.png" className="seta"/>
         </div>
     );
 }
